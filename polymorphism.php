80d7a362cd6192ab425525dcfb994b051d42cbd2
hapro955 <hapro955@gmail.com>
2018-03-29 16:37:25 +0700

    create class animals implements action, appearance

--- polymorphism.php
+++ polymorphism.php
@@ -6,4 +6,13 @@
 	interface appearance{
 		public function hair();
 	}
+
+	class animals implements action, appearance{
+		publci function run(){
+			echo "animals run";
+		}
+		public function hair(){
+			echo "bad hair";
+		}
+	}
 ?>
