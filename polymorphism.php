2624c333a6628fd484ab7ab29146a83f9e9e5793
hapro955 <hapro955@gmail.com>
2018-03-29 16:44:00 +0700

    test program

--- polymorphism.php
+++ polymorphism.php
@@ -1,24 +1,30 @@
 <?php
-	interface action{
-		public function run();
-	}
+interface action{
+	public function run();
+}
 
-	interface appearance{
-		public function hair();
-	}
+interface appearance{
+	public function hair();
+}
 
-	class animals implements action, appearance{
-		public function run(){
-			echo "animals run";
-		}
-		public function hair(){
-			echo "bad hair";
-		}
+class animals implements action, appearance{
+	public function run(){
+		echo "animals run";
 	}
+	public function hair(){
+		echo "bad hair";
+	}
+}
 
-	class human implements action{
-		public function run(){
-			echo "person run";
-		}
+class human implements action{
+	public function run(){
+		echo "person run";
 	}
+}
+
+$animal = new animals();
+echo $animal->run();
+echo $animal->hair();
+$human = new human();
+echo $human->run();
 ?>
