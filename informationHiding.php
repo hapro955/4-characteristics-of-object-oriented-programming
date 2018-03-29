44aa3329746de86505424554e6225b8bf440f694
hapro955 <hapro955@gmail.com>
2018-03-29 16:55:31 +0700

    create class bankAccount

--- informationHiding.php
+++ informationHiding.php
@@ -1,3 +1,18 @@
 <?php
+class bankAccount{
+	public $email;
+	public $pass;
+	protected $balance;
 
+	public $listAccount = [
+		'0' => [
+				 'email' => 'hapro955@gmail.com',
+				 'pass' => '123'
+			   ],
+		'1' => [
+				 'email' => 'thienmta@gmail.com',
+				 'pass' => '123'
+			   ]
+	];
+}
 ?>
